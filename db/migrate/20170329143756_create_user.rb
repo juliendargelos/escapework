2358933471fd32e2e924a9636b2ca69eae932b27
Julien Dargelos <julien.dargelos@me.com>
2017-03-29 17:06:57 +0200

    Unique email, password, firstname, lastname, and admin not null, admin has false as default value

--- db/migrate/20170329143756_create_user.rb
+++ db/migrate/20170329143756_create_user.rb
@@ -1,11 +1,11 @@
 class CreateUser < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
-      t.string :firstname
-      t.string :lastname
-      t.boolean :admin
+      t.string :email, null: false, unique: true
+      t.string :password_digest, null: false
+      t.string :firstname, null: false
+      t.string :lastname, null: false
+      t.boolean :admin, default: false
     end
   end
 end
