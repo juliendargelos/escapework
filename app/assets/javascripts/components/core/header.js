57336d7e54db209e4e9387ac27586ed8752b32f4
Julien Dargelos <julien.dargelos@me.com>
2017-03-29 21:41:00 +0200

    Immproves burger menu

--- app/assets/javascripts/components/core/header.js
+++ app/assets/javascripts/components/core/header.js
@@ -6,8 +6,16 @@ var header = {
 	nav: {
 		element: document.querySelector('body > header nav'),
 		toggler: document.querySelector('body > header .nav'),
+		transition: 600,
+		breakpoint: 800,
 		get isOpen() {
 			return this.element.getAttribute('data-open') !== null;
+		},
+		get isClosed() {
+			return this.element.getAttribute('data-close') !== null;
+		},
+		get hasState() {
+			return this.isOpen || this.isClosed;
 		},
 		open: function() {
 			this.element.setAttribute('data-open', '');
@@ -20,15 +28,27 @@ var header = {
 		toggle: function() {
 			return this.isOpen ? this.close() : this.open();
 		},
+		clear: function() {
+			this.element.removeAttribute('data-open');
+			this.element.removeAttribute('data-close');
+		},
 		get click() {
 			var self = this;
 			return function() {
 				self.toggle();
 			};
 		},
+		get resize() {
+			var self = this;
+
+			return function() {
+				if(self.hasState && window.innerWidth >= self.breakpoint) self.clear();
+			};
+		},
 		init: function() {
 			if(this.element && this.toggler) {
 				this.toggler.on('click', this.click);
+				window.on('resize', this.resize);
 			}
 		}
 	},
