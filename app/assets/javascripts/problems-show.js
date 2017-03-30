45c5eaf765699b7b1e4bec283eb3614276cf1c91
Sacha Andre <andresachaa@gmail.com>
2017-03-30 23:53:52 +0200

    Adding clues highlighting (JS)

---
+++ app/assets/javascripts/problems-show.js
@@ -0,0 +1,17 @@
+//= require components/core/main
+
+var problemsShow = {
+	clues: document.querySelectorAll('.content strong'),
+	click: function() {
+		this.setAttribute('data-found', '');
+	},
+	init: function() {
+		for(var i = 0; i < this.clues.length; i++) {
+			this.clues[i].on('click', this.click);
+		}
+	}
+};
+
+main.exec('problemsShow', [
+	'eventsManager'
+]);
